9998f6ad462ea2932cc9ec2e53002fae76060b1f
Evgeniia Brezhe <ebrezhe@healthjoy.com>
2023-10-15 12:08:11 +0200

    [Lesson 5] Fixes for models

--- hr/models.py
+++ hr/models.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
+from phone_field import PhoneField
 
 
 class Department(models.Model):
     name = models.CharField(max_length=200)
     parent_department = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -16,6 +18,7 @@ class Position(models.Model):
     department = models.ForeignKey('Department', on_delete=models.CASCADE)
     is_manager = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    job_description = models.CharField(max_length=200, blank=True)
 
     def save(self, *args, **kwargs):
         if self.is_manager:
@@ -32,3 +35,19 @@ class Employee(AbstractUser):
     hire_date = models.DateField(null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     position = models.ForeignKey('Position', on_delete=models.SET_NULL, null=True, blank=True)
+    phone_number = PhoneField(blank=True, help_text='Contact phone number')
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    tax_code = models.CharField(max_length=200)
+
+    def save(self, *args, **kwargs):
+        if not self.pk and Company.objects.exists():
+            raise ValidationError('There can be only one Company instance.')
+        return super(Company, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
