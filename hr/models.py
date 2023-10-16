1ab60fe866fe7810f9272fb16fd4c80698e50bc3
Hazcker35 <hazckers.net@gmail.com>
2023-10-16 20:54:23 +0100

    add company model,changed Employee and Position

--- hr/models.py
+++ hr/models.py
@@ -3,6 +3,22 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    email = models.EmailField()
+    tax_id = models.CharField(max_length=200)
+
+    def __str(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.pk and Company.objects.exists():
+            raise ValidationError('There can be only one Company instance')
+
+        return super(Company, self).save(*args, **kwargs)
+
+
 class Department(models.Model):
     name = models.CharField(max_length=200)
     parent_department = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
@@ -15,13 +31,16 @@ class Position(models.Model):
     title = models.CharField(max_length=200)
     department = models.ForeignKey('Department', on_delete=models.CASCADE)
     is_manager = models.BooleanField(default=False)
+    job_description = models.CharField(max_length=500, blank=True)
 
     def save(self, *args, **kwargs):
         if self.is_manager:
             existing_manager = Position.objects.filter(department=self.department, is_manager=True).exists()
+
             if existing_manager:
                 raise ValidationError(f'Manager already exists in the {self.department.name} department.')
-        super(Position, self).save(*args, **kwargs)
+
+        return super(Position, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
@@ -31,3 +50,4 @@ class Employee(AbstractUser):
     hire_date = models.DateField(null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     position = models.ForeignKey('Position', on_delete=models.SET_NULL, null=True, blank=True)
+    phone_number = models.CharField(max_length=151, blank=True)
