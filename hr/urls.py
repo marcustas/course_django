aa48ea80376148c46452862652eb4c085ad2e543
Альбина <albina@My-Mac.local>
2023-10-17 00:30:45 +0300

    added profile view,updated search,added pagination

--- hr/urls.py
+++ hr/urls.py
@@ -8,19 +8,5 @@ urlpatterns = [
     path('employees/create/', views.EmployeeCreateView.as_view(), name='employee_create'),
     path('employees/update/<int:pk>/', views.EmployeeUpdateView.as_view(), name='employee_update'),
     path('employees/delete/<int:pk>/', views.EmployeeDeleteView.as_view(), name='employee_delete'),
+    path('employees/profile/<int:pk>/', views.EmployeeProfileView.as_view(), name='employee_profile'),
 ]
-
-# from hr.views.function_views import (
-#     employee_create,
-#     employee_delete,
-#     employee_list,
-#     employee_update,
-# )
-#
-#
-# urlpatterns = [
-#     path('employees/', employee_list, name='employee_list'),
-#     path('employees/create/', employee_create, name='employee_create'),
-#     path('employees/update/<int:pk>/', employee_update, name='employee_update'),
-#     path('employees/delete/<int:pk>/', employee_delete, name='employee_delete'),
-# ]
