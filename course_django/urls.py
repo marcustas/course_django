90829be8003a05cc3b79b770b4518cf5f0381ffa
marcustas <stas.chernov@casafari.com>
2023-10-19 10:58:40 +0200

    Django querysets.py

--- course_django/urls.py
+++ course_django/urls.py
@@ -23,4 +23,7 @@ from django.urls import (
 urlpatterns = [
     path('', include('hr.urls')),
     path('napshhdf/', admin.site.urls),
+
+    # Examples
+    path('examples/', include('examples.urls'))
 ]
