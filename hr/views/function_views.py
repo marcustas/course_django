36710ac0f7310ad1b1ba85efd8da8dd9fa642b94
Evgeniia Brezhe <ebrezhe@healthjoy.com>
2023-10-25 12:34:35 +0200

    [Lesson 7] Created 5 querysets

--- hr/views/function_views.py
+++ hr/views/function_views.py
@@ -8,7 +8,7 @@ from django.shortcuts import (
 from django.urls import reverse
 
 from hr.forms import EmployeeForm
-from hr.models import Employee
+from hr.models import Employee, Department, Position
 
 
 def user_is_superadmin(user) -> bool:
@@ -66,3 +66,22 @@ def employee_delete(request, pk):
         employee.delete()
         return redirect(reverse('employee_list'))
     return render(request, 'employee_confirm_delete.html', {'object': employee})
+
+
+@user_passes_test(user_is_superadmin)
+def homework_querysets(request):
+    all_departments_with_managers = Department.objects.filter(position__is_manager=True).order_by('name')
+    count_active_positions = Position.objects.filter(is_active=True).count()
+    active_or_hr_positions = Position.objects.filter(department__name='HR') | Position.objects.filter(is_active=True)
+    departments_with_managers = Department.objects.filter(position__is_manager=True).values('name')
+    positions_sorted_by_name = Position.objects.all().order_by('title').values('title', 'is_manager')
+
+    context = {
+        'all_departments_with_managers': all_departments_with_managers,
+        'count_active_positions': count_active_positions,
+        'active_or_hr_positions': active_or_hr_positions,
+        'departments_with_managers': departments_with_managers,
+        'positions_sorted_by_name': positions_sorted_by_name,
+    }
+
+    return render(request, 'homework_querysets.html', context)
