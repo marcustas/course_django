29a22ee1b87f1c1f068bc1db25b5eb03d224305e
denis <d.protsenko87@gmail.com>
2023-10-29 17:03:52 +0200

    First

--- hr/forms.py
+++ hr/forms.py
@@ -42,3 +42,31 @@ class SalaryForm(forms.Form):
                     choices=WorkDayChoices,
                     initial=WorkDayEnum.WORKING_DAY.name,
                 )
+
+    def clean_employee(self):
+        employee = self.cleaned_data.get('employee')
+        if not employee:
+            raise forms.ValidationError("Поле Employee повинно бути заповнене.")
+        return employee
+
+    def clean(self):
+        cleaned_data = super().clean()
+        sick_days_count = 0
+        holiday_days_count = 0
+
+        for day in range(1, 32):
+            field_name = f'day_{day}'
+            work_day_status = cleaned_data.get(field_name)
+
+            if work_day_status == WorkDayEnum.SICK_DAY.name:
+                sick_days_count += 1
+            elif work_day_status == WorkDayEnum.HOLIDAY.name:
+                holiday_days_count += 1
+
+        if sick_days_count > 5:
+            raise forms.ValidationError("Кількість лікарняних днів не повинна перевищувати 5.")
+
+        if holiday_days_count > 3:
+            raise forms.ValidationError("Кількість днів відпочинку не повинна перевищувати 3.")
+
+        return cleaned_data
