424f652343872b9eef1cfc7d8bf186fbbc1d7ccb
Evgeniia Brezhe <ebrezhe@healthjoy.com>
2023-10-31 19:54:32 +0100

    [Lesson 9] Added process_exception() for Middleware and custom Management Command

---
+++ hr/management/commands/set_active_status_for_all_employees.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from hr.models import Employee
+
+
+class Command(BaseCommand):
+    help = 'Set "active" status for all Employees'
+
+    def handle(self, *args, **kwargs):
+        employees = Employee.objects.all()
+        employees.update(is_active=True)
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Successfully set "active" status for {employees.count()} Employees',
+            ),
+        )
