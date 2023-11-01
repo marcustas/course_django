a27e87ab1a87feafa086a67fa0ef4b32dcc0c689
Andrea <mateacademy.andrea@gmail.com>
2023-11-01 21:24:05 +0200

    home_work_4

--- hr/models.py
+++ hr/models.py
@@ -15,6 +15,7 @@ class Position(models.Model):
     title = models.CharField(max_length=200)
     department = models.ForeignKey('Department', on_delete=models.CASCADE)
     is_manager = models.BooleanField(default=False)
+    job_description = models.CharField(max_length=255)
 
     def save(self, *args, **kwargs):
         if self.is_manager:
@@ -31,3 +32,19 @@ class Employee(AbstractUser):
     hire_date = models.DateField(null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     position = models.ForeignKey('Position', on_delete=models.SET_NULL, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    email = models.EmailField()
+    tax_code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.pk and Company.objects.exists():
+            raise ValidationError('There can be only one Company instance.')
+        return super(Company, self).save(*args, **kwargs)
+
