b86d71c44508fbaa3a0fe11be1e6abc44b0bc5e8
ChernovMykola <4ernovnikola@gmail.com>
2023-11-03 15:14:17 +0100

    ProblemWithDepend

--- hr/translation.py
+++ hr/translation.py
@@ -3,11 +3,23 @@ from modeltranslation.translator import (
     translator,
 )
 
-from hr.models import Position
+from hr.models import (
+    Position,
+    Department,
+    Company,
+)
 
 
 class PositionTranslationOptions(TranslationOptions):
     fields = ('title', 'job_description')
 
 
-translator.register(Position, PositionTranslationOptions)
+class CompanyTranslationOptions(TranslationOptions):
+    fields = ('name', 'address')
+
+
+class DepartmentTranslationOptions(TranslationOptions):
+    fields = ('name', 'parent_department')
+
+
+translator.register(Position, PositionTranslationOptions, Company, CompanyTranslationOptions, Department, DepartmentTranslationOptions)
