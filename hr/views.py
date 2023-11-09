8e11a29e300a171b243dcb3e4f7316450e2d8336
Yaroslav <yar1k_k12@icloud.com>
2023-11-09 21:48:04 +0200

    Task 3

--- hr/views.py
+++ hr/views.py
@@ -77,6 +77,15 @@ class EmployeeUpdateView(UserIsAdminMixin, UpdateView):
     template_name = 'employee_form.html'
     success_url = reverse_lazy('hr:employee_list')
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Інформацію успішно відредаговано.')
+        return response
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Виникла помилка при редагувані.')
+        return super().form_invalid(form)
+
 
 class EmployeeDeleteView(UserIsAdminMixin, DeleteView):
     model = Employee
