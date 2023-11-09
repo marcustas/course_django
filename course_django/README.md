051ba9ffef9ab10353fae27b4520adcb69ebd0b3
Andrea <mateacademy.andrea@gmail.com>
2023-11-10 00:42:04 +0200

    home_work

---
+++ course_django/README.md
@@ -0,0 +1,2 @@
+# course_django
+Django sample project
---
+++ course_django/hr/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from modeltranslation.admin import TranslationAdmin
+
+from hr.models import (
+    Department,
+    Employee,
+    MonthlySalary,
+    Position,
+)
+
+
+@admin.register(Department)
+class DepartmentAdmin(TranslationAdmin):
+    list_display = ('name', 'parent_department')
+
+
+@admin.register(Position)
+class PositionAdmin(TranslationAdmin):
+    list_display = ('id', 'title', 'department', 'is_manager')
+
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.save()
+        except ValidationError as e:
+            form.add_error(None, e)
+            super().save_model(request, obj, form, change)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('username', 'position', 'hire_date', 'phone_number')
+
+
+@admin.register(MonthlySalary)
+class MonthlySalaryAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'month_year', 'salary', 'paid')
