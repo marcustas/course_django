e61d1671ac3da1ed57669a00f9d346a1fbd92df3
Альбина <albina@My-Mac.local>
2023-11-11 21:25:11 +0200

    homework_9-Vlasinko_Albina

--- hr/translation.py
+++ hr/translation.py
@@ -1,13 +1,19 @@
 from modeltranslation.translator import (
     TranslationOptions,
     translator,
+    register
 )
 
-from hr.models import Position
+from hr.models import Position, Department
 
 
 class PositionTranslationOptions(TranslationOptions):
     fields = ('title', 'job_description')
+
+
+@register(Department)
+class DepartmentTranslationOptions(TranslationOptions):
+    fields = ('name',)
 
 
 translator.register(Position, PositionTranslationOptions)
