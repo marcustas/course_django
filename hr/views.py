0717c73dbd391efd2139d21e3749f26867b00139
Альбина <albina@My-Mac.local>
2023-11-11 23:17:16 +0200

    homework_11-Vlasinko_Albina

--- hr/views.py
+++ hr/views.py
@@ -77,6 +77,15 @@ class EmployeeUpdateView(UserIsAdminMixin, UpdateView):
     template_name = 'employee_form.html'
     success_url = reverse_lazy('hr:employee_list')
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'User was updated!')
+        return response
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Ooops. Please, try again!')
+        return super().form_invalid(form)
+
 
 class EmployeeDeleteView(UserIsAdminMixin, DeleteView):
     model = Employee
