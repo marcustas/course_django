ae9b2b26932189e8dd176bd7fedad59d76625f2d
braviss <doctype.developer@gmail.com>
2023-11-18 10:20:06 +0200

    HW13 Kutovyi Viktor DRF_Additional

--- hr/validators.py
+++ hr/validators.py
@@ -4,3 +4,8 @@ from rest_framework import serializers
 def validate_positive(value):
     if value < 0:
         raise serializers.ValidationError('This field must be positive.')
+
+
+def validate_holiday_days(value):
+    if value > 10:
+        raise serializers.ValidationError('The number of holiday days cannot be more than 10.')
