3df6dc3443b36b29545bc426aee741d932355bc5
braviss <doctype.developer@gmail.com>
2023-11-27 22:17:54 +0200

    HW14 Kutovyi Viktor tests

--- hr/tests/test_employees_api.py
+++ hr/tests/test_employees_api.py
@@ -48,3 +48,42 @@ class EmployeeAPITestCase(APITestCase):
     def test_search_employee(self):
         response = self.client.get(reverse('api-hr:employee-list'), {'search': 'Test'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+
+class PositionViewSetTestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = Employee.objects.create_user(username='testuser', password='testpassword', email='test@gmail.com')
+        self.position = PositionFactory()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_positions_list(self):
+        response = self.client.get(reverse('api-hr:position-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+    def test_create_position(self):
+        data = {
+            'title': 'New Position Name',
+            'department': "1"
+        }
+        response = self.client.post(reverse('api-hr:position-list'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    def test_update_position(self):
+        position = PositionFactory()
+        data = {
+            'title': 'New Position Name',
+            'department': "1"
+        }
+        response = self.client.patch(reverse('api-hr:position-detail', kwargs={'pk': position.pk}), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_position(self):
+        position = PositionFactory()
+        response = self.client.delete(reverse('api-hr:position-detail', kwargs={'pk': position.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
