0c3deda6230a8f66f6e43312b198a23d5d01bb79
Альбина <albina@My-Mac.local>
2023-12-04 15:24:02 +0200

    homework-15_Vlasinko_Albina

--- hr/tasks.py
+++ hr/tasks.py
@@ -4,6 +4,8 @@ from hr.models import Employee
 from django.utils import timezone
 from datetime import timedelta
 
+from general.models import RequestStatistics
+
 
 @shared_task
 def send_password_reset_emails():
@@ -20,3 +22,32 @@ def send_password_reset_emails():
             [user.email],
             fail_silently=False,
         )
+
+
+@shared_task
+def generate_user_activity_report():
+    data = RequestStatistics.objects.all()
+
+    report = {}
+    for entry in data:
+        user_id = entry.user.id
+        username = entry.user.username
+        request_count = report.get(user_id, {}).get('request_count', 0) + 1
+        exception_count = report.get(user_id, {}).get('exception_count', 0) + int(entry.has_exception)
+        report[user_id] = {'username': username, 'request_count': request_count, 'exception_count': exception_count}
+
+    report_text = "User Report:\n"
+    for user_id, user_data in report.items():
+        report_text += f"Username: {user_data['username']}, Requests: {user_data['request_count']}, Exceptions: {user_data['exception_count']}\n"
+
+    subject = 'Daily Report'
+    message = report_text
+    from_email = 'your@email.com'
+    admin_emails = list(Employee.objects.filter(is_superuser=True).values_list('email'))
+
+    send_mail(subject,
+              message,
+              from_email,
+              admin_emails,
+              fail_silently=False,
+              )
