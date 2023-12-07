d99f2b123542b07f8693c7fa6e5c879eac68597f
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-12-07 11:52:36 +0200

    additional translations were added

--- hr/apps.py
+++ hr/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class HrConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hr'
+    verbose_name = _('HR application')
