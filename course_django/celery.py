256cfa3b4f11e684e2cc2fa618cb253e8609cb57
Andrea <mateacademy.andrea@gmail.com>
2023-12-07 23:34:13 +0200

    home_work_15

--- course_django/celery.py
+++ course_django/celery.py
@@ -1,5 +1,6 @@
 from celery import Celery
 import os
+from datetime import timedelta
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'course_django.settings')
 
@@ -8,3 +9,11 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.broker_url = 'redis://redis:6379/0'
 app.conf.beat_scheduler = 'django_celery_beat.schedulers:DatabaseScheduler'
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'send-activity-report': {
+        'task': 'path.to.send_activity_report',
+        'schedule': timedelta(days=1),
+        'args': (),
+    },
+}
--- hr/tasks.py
+++ hr/tasks.py
@@ -3,7 +3,13 @@ from django.core.mail import send_mail
 from hr.models import Employee
 from django.utils import timezone
 from datetime import timedelta
+from django.db import models
 
+class RequestStatistics(models.Model):
+    user = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    request_count = models.IntegerField(default=0)
+    exception_count = models.IntegerField(default=0)
+    date = models.DateField(auto_now_add=True)
 
 @shared_task
 def send_password_reset_emails():
@@ -18,5 +24,29 @@ def send_password_reset_emails():
             message,
             'from@example.com',
             [user.email],
+            fail_silently=False,
+        )
+@shared_task
+def send_activity_report():
+    yesterday = timezone.now().date() - timedelta(days=1)
+    users = User.objects.all()
+
+    for user in users:
+        statistics, created = RequestStatistics.objects.get_or_create(
+            user=user,
+            date=yesterday
+        )
+
+        request_count = statistics.request_count
+        exception_count = statistics.exception_count
+
+        subject = f"Звіт про активність користувача {user.username}"
+        message = f"Ім'я користувача: {user.username}\nКількість запитів: {request_count}\nКількість винятків: {exception_count}"
+
+        send_mail(
+            subject,
+            message,
+            'from@example.com',
+            ['admin@example.com'],
             fail_silently=False,
         )
