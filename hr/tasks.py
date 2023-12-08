36c97f0afc8cfe990e9eccd0234ca20cca217f8a
Andrea <mateacademy.andrea@gmail.com>
2023-12-08 22:41:51 +0200

    home_work_15

--- hr/tasks.py
+++ hr/tasks.py
@@ -4,13 +4,7 @@ from hr.models import Employee
 from django.utils import timezone
 from datetime import timedelta
 from django.db import models
-
-class RequestStatistics(models.Model):
-    user = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    request_count = models.IntegerField(default=0)
-    exception_count = models.IntegerField(default=0)
-    date = models.DateField(auto_now_add=True)
-
+from general.models import RequestStatistics
 @shared_task
 def send_password_reset_emails():
     next_month = timezone.now().date() + timedelta(days=30)
@@ -37,8 +31,8 @@ def send_activity_report():
             date=yesterday
         )
 
-        request_count = statistics.request_count
-        exception_count = statistics.exception_count
+        request_count = statistics.requests
+        exception_count = statistics.exception
 
         subject = f"Звіт про активність користувача {user.username}"
         message = f"Ім'я користувача: {user.username}\nКількість запитів: {request_count}\nКількість винятків: {exception_count}"
