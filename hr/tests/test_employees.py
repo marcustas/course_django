c107e6827d77637bf28896414dc5491801617c62
UlyaM17 <forsoc310@gmail.com>
2023-12-10 18:39:31 -0600

    hw 14

--- hr/tests/test_employees.py
+++ hr/tests/test_employees.py
@@ -15,9 +15,8 @@ from hr.tests.factories import (
 class EmployeeListViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.employees_count = 10
-        self.employees = EmployeeFactory.create_batch(self.employees_count)
-        self.client.force_login(user=self.employees[0])
+        self.employees = EmployeeFactory.create_batch(10)
+        self.client.force_login(self.employees[0])
         self.url = reverse('hr:employee_list')
 
     def test_access_employee_list(self):
@@ -27,12 +26,12 @@ class EmployeeListViewTest(TestCase):
     def test_employee_list_content(self):
         response = self.client.get(self.url)
         self.assertTrue('employees' in response.context)
-        self.assertEqual(len(response.context['employees']), self.employees_count)
+        self.assertEqual(len(response.context['employees']), 10)
 
     def test_employee_search(self):
         search_query = self.employees[0].first_name
         response = self.client.get(f'{self.url}?search={search_query}')
-        self.assertEqual(len(response.context['employees']), 1)
+        self.assertTrue(len(response.context['employees']), 1)
         self.assertEqual(response.context['employees'][0].first_name, search_query)
 
 
@@ -87,3 +86,42 @@ class EmployeeCreateViewTest(TestCase):
         messages = list(get_messages(response.wsgi_request))
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Працівника успішно створено.')
+
+
+class EmployeeProfileViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.employee = EmployeeFactory()
+        self.url = reverse('hr:employee_profile', args=[self.employee.id])
+
+    def test_profile_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'hr/employee_profile.html')
+        self.assertEqual(response.context['employee'], self.employee)
+
+
+class EmployeeDeleteViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.employee = EmployeeFactory()
+        self.url = reverse('hr:employee_delete', args=[self.employee.id])
+
+    def test_delete_view(self):
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Employee.objects.filter(id=self.employee.id).exists())
+
+
+class EmployeeUpdateViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.employee = EmployeeFactory()
+        self.url = reverse('hr:employee_update', args=[self.employee.id])
+
+    def test_update_view(self):
+        new_first_name = 'UpdatedFirstName'
+        response = self.client.post(self.url, {'first_name': new_first_name})
+        self.assertEqual(response.status_code, 302)
+        updated_employee = Employee.objects.get(id=self.employee.id)
+        self.assertEqual(updated_employee.first_name, new_first_name)
