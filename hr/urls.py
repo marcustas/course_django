168d990cd5bd81aa11465174dfbe63f42193b98a
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-12-26 22:04:33 +0100

    Add homework_querysets view to urls.py

--- hr/urls.py
+++ hr/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from hr.views import generic_views as views
+from hr.views.function_views import homework_querysets
 
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path('employees/update/<int:pk>/', views.EmployeeUpdateView.as_view(), name='employee_update'),
     path('employees/delete/<int:pk>/', views.EmployeeDeleteView.as_view(), name='employee_delete'),
     path('employees/profile/<int:pk>/', views.EmployeeProfileView.as_view(), name='employee_profile'),
+    path('homework_querysets/', homework_querysets, name='homework_querysets'),
 ]
