42c94bd76e0ff773da82104b79c804bde18c2cd7
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-12-26 22:04:05 +0100

    Add homework_querysets view

--- hr/views/function_views.py
+++ hr/views/function_views.py
@@ -1,3 +1,7 @@
+from django.http import JsonResponse
+from django.core.serializers import serialize
+import json
+
 from django.contrib.auth.decorators import user_passes_test
 from django.db.models import Q
 from django.shortcuts import (
@@ -8,7 +12,7 @@ from django.shortcuts import (
 from django.urls import reverse
 
 from hr.forms import EmployeeForm
-from hr.models import Employee
+from hr.models import Company, Department, Position, Employee
 
 
 def user_is_superadmin(user) -> bool:
@@ -66,3 +70,24 @@ def employee_delete(request, pk):
         employee.delete()
         return redirect(reverse('employee_list'))
     return render(request, 'employee_confirm_delete.html', {'object': employee})
+
+
+def homework_querysets(request):
+    response_dict = {
+    # 1. Все Department, в которых есть позиция менеджер. Сортировка по названию департамента.
+    'first_queryset': json.loads(serialize('json', Department.objects.filter(position__title__icontains='manager').order_by('name'))),
+
+    # 2. Общее количество активных позиций (Position).
+    'second_queryset': Position.objects.filter(is_active=True).count(),
+
+    # 3. Все позиции, которые активны, или принадлежат к департаменту 'HR'.
+    'third_queryset': json.loads(serialize('json', Position.objects.filter(Q(is_active=True) | Q(department__name='HR')))),
+
+    # 4. Все департаменты в которых есть менеджеры. Использовать filter() и values().
+    'fourth_queryset': list(Department.objects.filter(position__is_manager=True).values('name')),
+
+    # 5. Все позиции отсортированные по названию, но выводить только title и is_active.
+    'fifth_queryset': list(Position.objects.order_by('title').values('title', 'is_active')),
+    }
+
+    return JsonResponse(response_dict, safe=False)
