b5fcdbfc76c31a883b31e6076cee91ef0fbd3753
Ruslan <agaham57@gmail.com>
2024-01-09 16:27:24 +0200

    HW13

--- hr/permissions.py
+++ hr/permissions.py
@@ -10,3 +10,8 @@ class IsNotRussianEmail(permissions.BasePermission):
         if request.user and request.user.email:
             return not request.user.email.endswith('.ru')
         return False
+
+class HasPosition(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj) -> bool:
+        return bool(request.user.position)
